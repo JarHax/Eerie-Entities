199774e4d6ba58d78cc024e6ba61c295d7cfa65b
Tyler Hancock <darklime@live.ca>
2018-10-03 21:15:25 -0600

    Added spawn eggs to the creative tab.

--- src/main/java/com/jarhax/spooky/CreativeTabSpooky.java
+++ src/main/java/com/jarhax/spooky/CreativeTabSpooky.java
@@ -2,18 +2,38 @@ package com.jarhax.spooky;
 
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.init.Blocks;
+import net.minecraft.init.Items;
+import net.minecraft.item.ItemMonsterPlacer;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.NonNullList;
+import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 
 public class CreativeTabSpooky extends CreativeTabs {
+    
+    public CreativeTabSpooky() {
+        
+        super(SpookyMod.MODID);
+    }
+    
+    @Override
+    public ItemStack createIcon () {
+        
+        return new ItemStack(Blocks.LIT_PUMPKIN);
+    }
+    
+    @Override
+    @SideOnly(Side.CLIENT)
+    public void displayAllRelevantItems(NonNullList<ItemStack> itemList) {
 
-	public CreativeTabSpooky() {
-		
-		super(SpookyMod.MODID);
-	}
-
-	@Override
-	public ItemStack createIcon() {
-
-		return new ItemStack(Blocks.LIT_PUMPKIN);
-	}
+        super.displayAllRelevantItems(itemList);
+        
+        for (ResourceLocation id : SpookyMod.REGISTRY.getEntityIds()) {
+            
+            final ItemStack spawner = new ItemStack(Items.SPAWN_EGG);
+            ItemMonsterPlacer.applyEntityIdToItemStack(spawner, id);
+            itemList.add(spawner);
+        }
+    }
 }
