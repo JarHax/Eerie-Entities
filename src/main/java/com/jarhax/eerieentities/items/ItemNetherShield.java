8b24e0e5aee38870e677afb5e5487f3543fdaef5
Tyler Hancock <darklime@live.ca>
2018-10-13 00:14:23 -0600

    Added fire effect to the nether shield

--- src/main/java/com/jarhax/eerieentities/items/ItemNetherShield.java
+++ src/main/java/com/jarhax/eerieentities/items/ItemNetherShield.java
@@ -1,9 +1,38 @@
 package com.jarhax.eerieentities.items;
 
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.projectile.EntityArrow;
+import net.minecraft.item.EnumRarity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.DamageSource;
+
 public class ItemNetherShield extends ItemShieldBase {
     
     public ItemNetherShield() {
         
-        this.setMaxDamage(500);
+        this.setMaxDamage(512);
+    }
+    
+    @Override
+    public EnumRarity getRarity(ItemStack stack) {
+        
+        return EnumRarity.RARE;
+    }
+    
+    @Override
+    public void blockDamage(EntityLivingBase user, DamageSource damageSource, float amount) {
+        
+        final Entity source = damageSource.getImmediateSource();
+        
+        if (source != null) {
+            
+            source.setFire(2);
+            
+            if (source instanceof EntityArrow) {
+                
+                source.setFire(30);
+            }
+        }
     }
 }
