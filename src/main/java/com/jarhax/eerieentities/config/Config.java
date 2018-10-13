4ce529e97181400c3ea3d5426bbf18078d910edd
Tyler Hancock <darklime@live.ca>
2018-10-13 01:07:18 -0600

    Adjust wisp config to increase spawn rate and decrease EXP.

--- src/main/java/com/jarhax/eerieentities/config/Config.java
+++ src/main/java/com/jarhax/eerieentities/config/Config.java
@@ -12,7 +12,7 @@ public class Config {
     
     public static Configuration cfg = new Configuration("eerieentities");
     
-    public static MobConfig wisp = new MobConfig("will_o_wisp", EntityWisp.class, EnumCreatureType.AMBIENT, 1d, 0.2d, 0d, 0d, 4, 1, 1, 1, 1, "type=SWAMP");
+    public static MobConfig wisp = new MobConfig("will_o_wisp", EntityWisp.class, EnumCreatureType.AMBIENT, 1d, 0.2d, 0d, 0d, 4, 1, 1, 40, 1, "type=SWAMP");
     public static MobConfigPumpkinSlime pumpkinSlime = new MobConfigPumpkinSlime(10d, 0.4d, 0d, 4d, 4, 1, 1, 5, 3, "type=MAGICAL", "type=FOREST", "type=PLAINS");
     public static MobConfigNetherKnight netherKnight = new MobConfigNetherKnight(100d, 0.23d, 5d, 5d, 1, 1, 1, 1, 350, "type=NETHER");
     public static MobConfigCursedArmor cursedArmor = new MobConfigCursedArmor(1d, 0.2d, 0d, 1d, 1, 1, 1, 1, 15, "type=WASTELAND", "type=SPOOKY", "type=DEAD", "type=MAGICAL");
