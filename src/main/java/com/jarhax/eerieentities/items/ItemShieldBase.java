805757eaeab1f410fba9daad144be01afb4132d7
Tyler Hancock <darklime@live.ca>
2018-10-13 00:14:08 -0600

    Added a hook for detecting when a shield does damage.

--- src/main/java/com/jarhax/eerieentities/items/ItemShieldBase.java
+++ src/main/java/com/jarhax/eerieentities/items/ItemShieldBase.java
@@ -1,6 +1,7 @@
 package com.jarhax.eerieentities.items;
 
 import net.minecraft.block.BlockDispenser;
+import net.minecraft.entity.EntityLiving;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.EnumAction;
@@ -8,12 +9,18 @@ import net.minecraft.item.Item;
 import net.minecraft.item.ItemArmor;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.ActionResult;
+import net.minecraft.util.DamageSource;
 import net.minecraft.util.EnumActionResult;
 import net.minecraft.util.EnumHand;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.world.World;
+import net.minecraftforge.event.entity.living.LivingAttackEvent;
+import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
 // TODO move this class to bookshelf.
+// TODO add many more hooks into Nautilus
+@EventBusSubscriber
 public class ItemShieldBase extends Item {
     
     public ItemShieldBase() {
@@ -47,5 +54,20 @@ public class ItemShieldBase extends Item {
     public boolean isShield (ItemStack stack, EntityLivingBase entity) {
         
         return true;
+    }
+
+    public void blockDamage(EntityLivingBase user, DamageSource damageSource, float amount) {
+        
+        // Hook for when damage is blocked.
+    }
+    
+    @SubscribeEvent
+    public static void onAttack(LivingAttackEvent event) {
+        
+        if (event.getEntityLiving().getActiveItemStack().getItem() instanceof ItemShieldBase && !event.getSource().isUnblockable()) {
+            
+            ItemShieldBase shield = (ItemShieldBase) event.getEntityLiving().getActiveItemStack().getItem();
+            shield.blockDamage(event.getEntityLiving(), event.getSource(), event.getAmount());
+        }
     }
 }
